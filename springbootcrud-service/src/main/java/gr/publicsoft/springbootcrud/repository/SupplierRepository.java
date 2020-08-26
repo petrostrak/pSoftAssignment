eaa8a2d7f42c8f9111b674f127e0d50581482ebe
petros <pit.trak@gmail.com>
2020-08-26 23:37:39 +0300

    implemented crudrepository

--- springbootcrud-service/src/main/java/gr/publicsoft/springbootcrud/repository/SupplierRepository.java
+++ springbootcrud-service/src/main/java/gr/publicsoft/springbootcrud/repository/SupplierRepository.java
@@ -4,8 +4,8 @@ import gr.publicsoft.springbootcrud.model.Supplier;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,18 +16,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
  */
 @CrossOrigin(origins = "http://localhost:9000")
 @RepositoryRestResource
-public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+public interface SupplierRepository extends CrudRepository<Supplier, Long> {
     
     //Supplier findByCompanyName(String companyName);
     //Supplier findByVatNumber(String vatNumber);
     //List<Supplier> findById(long id);
     
     @Query("SELECT s FROM Supplier s "
-            + "WHERE s.vatNumber LIKE CONCAT('%',?1,'%')  "
-            + "     OR s.companyName LIKE CONCAT('%',?1,'%')")
+            + "WHERE s.companyName LIKE CONCAT('%',?1,'%') "
+            + "     OR s.vatNumber LIKE CONCAT('%',?1,'%')")
     Page<Supplier> findByQuery(@Param("query") String query, Pageable pageable);
-    
-    @Query("SELECT COUNT(s) FROM Supplier s "
-            + "WHERE s.companyName IS NOT NULL")
-    Long countAllSuppliers();
 }
